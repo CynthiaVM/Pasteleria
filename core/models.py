ccae589ded2566341ee25bb927cbf0cdfa6958f0
CynthiaVM <cynthiamouzo@hotmail.com>
2021-10-03 23:57:11 -0300

    intento

---
+++ core/models.py
@@ -0,0 +1,16 @@
+from django.db.models import Model, CharField, TextField, DateTimeField, ImageField
+
+class Dessert(Model):
+    title= CharField(max_length=540, verbose_name="Titulo")
+    description=TextField(verbose_name= "Descripcion")
+    image=ImageField(upload_to= "Dessert")
+    created= DateTimeField(auto_now_add=True)
+    updated= DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name= "Postre"
+        verbose_name_plural= "Postres"
+
+    def __str__(self) -> str:
+        return self.title
+
